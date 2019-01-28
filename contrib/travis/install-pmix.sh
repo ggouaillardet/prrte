991c2cdd2b4cb33f652ccab35b81ef06da681d72
Gilles Gouaillardet <gilles@rist.or.jp>
2019-01-28 13:00:42 +0900

    travis: test various PMIx releases/branches
     - v3.0, v3.1, and master branches
     - all releases from 3.0.0 except 3.1.0 that was DOA
    
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- contrib/travis/install-pmix.sh
+++ contrib/travis/install-pmix.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -ex
-git clone --depth=1 https://github.com/pmix/pmix pmix-master
-cd pmix-master
-./autogen.pl && ./configure --prefix=/usr --disable-picky $* && make && sudo make install
+if [ -n "$PMIX_BRANCH" ]; then
+    git clone --depth=1 -b $PMIX_BRANCH https://github.com/pmix/pmix pmix-$PMIX_BRANCH
+    cd pmix-$PMIX_BRANCH
+    ./autogen.pl && ./configure --prefix=/usr --disable-picky $* && make && sudo make install
+elif [ -n "$PMIX_RELEASE" ]; then
+    wget https://github.com/pmix/pmix/releases/download/v${PMIX_RELEASE}/pmix-${PMIX_RELEASE}.tar.bz2 && tar xvfj pmix-${PMIX_RELEASE}.tar.bz2
+    cd pmix-${PMIX_RELEASE}
+    ./configure --prefix=/usr --disable-picky $* && make && sudo make install
+else
+    echo no PMIX_BRANCH nor PMIX_RELEASE, check the environment/travis matrix
+fi
